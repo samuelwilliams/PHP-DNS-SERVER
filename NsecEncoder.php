ee951944823abc570378bc72d7674116f02f1186
Sam Williams <samuel.williams8@gmail.com>
2019-10-17 10:12:48 +1100

    Update NsecEncoder.php

--- NsecEncoder.php
+++ NsecEncoder.php
@@ -1,120 +1,63 @@
 <?php
 
 
+
 class NsecEncoder
 {
-    private static function getWindow(int $int)
+    public static function encode($types)
     {
-        return $int >> 8;
-    }
-
-    private static function getBitToFlip(int $int)
-    {
-        return $int & 0b11111111;
-    }
-
-    private static function flipBit(string &$mask, int $bit)
-    {
-        if (strlen($mask) < $bit) {
-            $diff = $bit - strlen($mask);
-            $mask .= str_repeat('0', $diff);
+        $blocks = [];
+    
+        foreach($types as $int) {
+            $window = $int >> 8;
+            $mask = $blocks[$window] ?? str_repeat("\0", 32)
+            $int = $int & 0b11111111;
+            $mod = $int % 8;
+            $byteNum = ($int - $mod) / 8;
+            $byte = ord($mask[$byteNum]) | (128 >> $mod);
+            $mask[$byteNum] = chr($byte);
+            $blocks[$window] = $mask;
         }
-
-        $mask[$bit] = '1';
-    }
-
-    private static function pad(string &$mask)
-    {
-        $mod = strlen($mask) % 8;
-        if (0 === $mod) {
-            return $mask;
-        }
-
-        $padlen = strlen($mask) + 8 - $mod;
-
-        $mask = str_pad($mask, $padlen, '0', STR_PAD_RIGHT);
-    }
-
-    private static function maskLen(string &$mask)
-    {
-        $len = strlen($mask) / 8;
-        $len = decbin((int) $len);
-        $len = str_pad($len, 8, '0', STR_PAD_LEFT);
-
-        $mask = $len.$mask;
-    }
-
-    private static function addToWindowBlocks(int $int, array &$windowBlocks)
-    {
-        $window = $int >> 8; //self::getWindow($int);
-        $pos = $int & 0b11111111; //self::getBitToFlip($int);
-
-        if (!array_key_exists($window, $windowBlocks)) {
-            $windowBlocks[$window] = '0';
-        }
-
-        $mask = $windowBlocks[$window];
-        self::flipBit($mask, $pos);
-
-        $windowBlocks[$window] = $mask;
-    }
-
-    public static function encode(array $types)
-    {
-        $windowBlocks = [];
-
-        foreach ($types as $type) {
-            self::addToWindowBlocks($type, $windowBlocks);
-        }
-
+    
         $encoded = '';
+        foreach($blocks as $n => $mask) {
+            $mask = rtrim($mask, "\0");
+            $encoded .= chr($n) . chr(strlen($mask)) . $mask;
+        }
+    
+        return $encoded;
+    }
+}
 
-        foreach ($windowBlocks as $block => &$mask) {
-            self::pad($mask);
-            self::maskLen($mask);
-            $block = str_pad(decbin($block), 8, '0', STR_PAD_LEFT);
-            $encoded .= $block.$mask;
+class NsecDecoder
+{
+    public static function decode($encoded)
+    {
+        $bytes = unpack('C*', $encoded);
+        $types = [];
+
+        while (false !== $window = current($bytes)) {
+            $mask = '';
+            $len = next($bytes);
+
+            for ($i = 0; $i < $len; $i++) {
+                $mask .= str_pad(decbin(next($bytes)), 8, '0', STR_PAD_LEFT);
+            }
+
+            $offset = 0;
+            while (false !== $pos = strpos($mask, '1', $offset)) {
+                $types[] = $window * 256 + $pos;
+                $offset = $pos + 1;
+            }
+
+            next($bytes);
         }
 
-        $bytes = array_map('bindec', str_split($encoded, 8));
-
-        return pack('C*', ...$bytes);
+        return $types;
     }
 }
 
 $types = [1,15,46,47,1234];
 $encoded = NsecEncoder::encode($types);
-
-$bytes = unpack('C*', $encoded);
-
-foreach ($bytes as $byte) {
-    echo '0x'.dechex($byte).' ';
-}
-//var_dump($bytes);
-$iterator = new \ArrayIterator($bytes);
-
-$blockWindows = [];
-
-while ($iterator->valid()) {
-    $window = $iterator->current();
-    $mask = '';
-
-    $iterator->next();
-    $len = $iterator->current();
-    $nextWindow = $iterator->key() + $len;
-
-    $iterator->next();
-
-    echo "$len, $nextWindow\n";
-
-    for ($i=0;$i<$len;$i++) {
-        $mask .= str_pad(decbin($iterator->current()), 8, '0', STR_PAD_LEFT);
-        $iterator->next();
-    }
-
-    $blockWindows[$window] = $mask;
-}
-
-
-
-var_dump($blockWindows);
+$decoded = NsecDecoder::decode($encoded);
+var_dump($decoded);
